80497ae1daf383ac3a597fd4514ab1285dbed0e7
Anderson B <andbmarques@gmail.com>
2022-11-29 17:46:19 -0300

    Add Feature: Home Screen Page

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,33 +1,56 @@
-import React from 'react';
-import { View, Text, StyleSheet, ScrollView, FlatList, Button, Pressable } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 export default function Home() {
+
+    const [data, setData] = useState([
+        { key: '1', title: 'Churras Aniversário', peoples: 8, value: 200 },
+        { key: '2', title: 'Churras Fim de Semana', peoples: 3, value: 100 }
+    ])
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
                 <AntDesign name='calculator' style={styles.logoIcon} />
-                <Text style={styles.title}>Churras<Text style={{fontWeight: 'bold'}}>Calc</Text></Text>
+                <Text style={styles.title}>Churras<Text style={{ fontWeight: 'bold' }}>Calc</Text></Text>
             </View>
             <ScrollView style={styles.content}>
-                <Text style={{fontWeight: 'bold', fontSize: 20}}>Meus churras</Text>
-                
+                <Text style={{ fontWeight: 'bold', fontSize: 20, marginBottom: 20 }}>Meus churras</Text>
+                {
+                    data.map(item => (
+                        <TouchableOpacity key={item.key} style={styles.itemContainer} >
+                            <Text style={styles.itemTitle}>{item.title}</Text>
+                            <View style={styles.itemIconsContainer}>
+                                <View style={styles.itemIconsContent}>
+                                    <AntDesign name="user" size={24} color="black" />
+                                    <Text style={styles.itemText} >{item.peoples}</Text>
+                                </View>
+                                <View style={styles.itemIconsContent}>
+                                    <AntDesign name="wallet" size={24} color="black" />
+                                    <Text style={styles.itemText}>R${item.value}</Text>
+                                </View>
+                            </View>
+                        </TouchableOpacity>
+                    ))
+                }
             </ScrollView>
-            <Pressable style={styles.addButton}>
-                <Text>+</Text>
-            </Pressable>
+            <TouchableOpacity style={styles.addButton}>
+                <Text style={{ color: '#ffffff', fontWeight: 'bold', fontSize: 25 }}>+</Text>
+            </TouchableOpacity>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
+    //PAGE STYLE
     container: {
         flex: 1,
         flexDirection: 'column',
         height: '100%',
     },
     header: {
-        height: 150,
+        height: 120,
         backgroundColor: '#F77F00',
         borderBottomWidth: 6,
         borderBottomColor: '#DDDDDD',
@@ -52,9 +75,42 @@ const styles = StyleSheet.create({
         position: 'absolute',
         right: 0,
         bottom: 0,
-        borderRadius: 100,
-        backgroundColor: 'red',
+        borderRadius: 20,
+        backgroundColor: '#F77F00',
         margin: 20,
-        padding: 10
-    }  
+        width: 40,
+        height: 40,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    //LIST ITEMS STYLES
+    itemContainer: {
+        backgroundColor: '#DDD',
+        borderLeftWidth: 8,
+        borderLeftColor: '#003049',
+        borderRadius: 4,
+        height: 100,
+        padding: 20,
+        flexDirection: 'column',
+        marginBottom: 10
+    },
+    itemTitle: {
+        fontWeight: 'bold',
+        fontSize: 18
+    },
+    itemIconsContainer: {
+        height: 35,
+        marginTop: 5,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    itemIconsContent: {
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    itemText: {
+        marginRight: 30,
+        marginLeft: 10
+    }
 })
